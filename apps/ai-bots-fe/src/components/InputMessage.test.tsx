48b121892209316a948e20c7c3a5c16d330ba2a5
Pavel Fokin <fokinpv@gmail.com>
2024-05-13 16:27:10 +0200

    [fe] Send message on 'Enter'

--- apps/ai-bots-fe/src/components/InputMessage.test.tsx
+++ apps/ai-bots-fe/src/components/InputMessage.test.tsx
@@ -30,3 +30,48 @@ test('calls handleSend with correct message on send click', () => {
     expect(handleSendMock).toHaveBeenCalledWith({ sender, text });
     expect(inputElement.value).toBe('');
 });
+
+test('calls handleSend with correct message on enter key press', () => {
+    const handleSendMock = vi.fn();
+    const sender = 'User';
+    const text = 'Hello, world!';
+
+    render(<InputMessage handleSend={handleSendMock} />);
+
+    const inputElement = screen.getByPlaceholderText('Type a message') as HTMLInputElement;
+    const form = screen.getByRole('form');
+
+    fireEvent.change(inputElement, { target: { value: text } });
+    // fireEvent.keyDown(inputElement, { key: 'Enter', code: 13, charCode: 13 });
+    fireEvent.submit(form);
+
+    expect(handleSendMock).toHaveBeenCalledWith({ sender, text });
+    expect(inputElement.value).toBe('');
+});
+
+test('does not call handleSend on empty message send click', () => {
+    const handleSendMock = vi.fn();
+
+    render(<InputMessage handleSend={handleSendMock} />);
+
+    const sendButton = screen.getByRole('button');
+
+    fireEvent.click(sendButton);
+
+    expect(handleSendMock).not.toHaveBeenCalled();
+});
+
+test('does not call handleSend on whitespace message send click', () => {
+    const handleSendMock = vi.fn();
+
+    render(<InputMessage handleSend={handleSendMock} />);
+
+    const inputElement = screen.getByPlaceholderText('Type a message') as HTMLInputElement;
+    const sendButton = screen.getByRole('button');
+
+    fireEvent.change(inputElement, { target: { value: ' ' } });
+    fireEvent.click(sendButton);
+
+    expect(handleSendMock).not.toHaveBeenCalled();
+});
+
--- apps/ai-bots-fe/src/components/InputMessage.tsx
+++ apps/ai-bots-fe/src/components/InputMessage.tsx
@@ -16,30 +16,33 @@ function InputMessage({ handleSend }: InputMessageProps) {
         setInputMessage({ ...inputMessage, text: event.target.value });
     }
 
-    const onSendClick = async () => {
-        if (inputMessage) {
-            handleSend({ sender: 'User', text: inputMessage.text});
+    const onSendClick = async (e: React.FormEvent) => {
+        e.preventDefault();
+        if (inputMessage.text.trim() !== ''){
+            handleSend({ sender: 'User', text: inputMessage.text });
             setInputMessage({ sender: '', text: '' });
         }
     };
 
     return (
-        <Flex gap="2" justify="center" p={{
-            initial: '2',
-            sm: '4',
-        }}>
-            <Box flexGrow="1">
-                <TextField.Root
-                    value={inputMessage.text}
-                    placeholder="Type a message"
-                    onChange={onInputChange}
-                    size="3"
-                />
-            </Box>
-            <IconButton size="3" onClick={onSendClick} highContrast>
-                <IconSend size={16} />
-            </IconButton>
-        </Flex>
+        <form role="form" onSubmit={onSendClick} >
+            <Flex gap="2" justify="center" p={{
+                initial: '2',
+                sm: '4',
+            }}>
+                <Box flexGrow="1">
+                    <TextField.Root
+                        value={inputMessage.text}
+                        placeholder="Type a message"
+                        onChange={onInputChange}
+                        size="3"
+                    />
+                </Box>
+                <IconButton size="3" onClick={onSendClick} highContrast>
+                    <IconSend size={16} />
+                </IconButton>
+            </Flex>
+        </form>
     );
 }
 
