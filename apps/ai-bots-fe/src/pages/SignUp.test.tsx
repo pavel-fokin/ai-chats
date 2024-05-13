d4ffd251576ed23b7d07a23876176f97fd0d1207
Pavel Fokin <fokinpv@gmail.com>
2024-05-13 15:32:15 +0200

    [fe] Add unit tests for SignIn, SignUp pages

---
+++ apps/ai-bots-fe/src/pages/SignUp.test.tsx
@@ -0,0 +1,73 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { HttpResponse, http } from 'msw';
+import { setupServer } from 'msw/node';
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
+
+import { AuthContextProvider } from "contexts";
+import { SignUp } from './SignUp';
+
+const server = setupServer(
+    http.post('/api/auth/signup', () => {
+        return HttpResponse.json({ data: { accessToken: 'accessToken' } });
+    }),
+);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+test('renders Sign Up component', () => {
+    render(
+        <AuthContextProvider>
+            <MemoryRouter initialEntries={['/app/signup']}>
+                <Routes>
+                    <Route path="/app/signup" element={
+                        <SignUp />
+                    } />
+                </Routes>
+            </MemoryRouter>
+        </AuthContextProvider>
+    );
+
+    expect(screen.getByRole('heading', { name: 'Sign Up' })).toBeInTheDocument();
+    expect(screen.getByPlaceholderText('Your username')).toBeInTheDocument();
+    expect(screen.getByPlaceholderText('Your password')).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: 'Create an account' })).toBeInTheDocument();
+    expect(screen.getByText('Already have an account?')).toBeInTheDocument();
+});
+
+test('calls signUp function and navigates to /app on successful sign up', async () => {
+    render(
+        <AuthContextProvider>
+            <MemoryRouter initialEntries={['/app/signup']}>
+                <Routes>
+                    <Route path="/app/signup" element={
+                        <SignUp />
+                    } />
+                    <Route path="/app" element={<div>App</div>} />
+                </Routes>
+            </MemoryRouter>
+        </AuthContextProvider>
+    );
+
+    const username = 'user';
+    const password = 'password';
+
+    const usernameInput = screen.getByPlaceholderText('Your username');
+    const passwordInput = screen.getByPlaceholderText('Your password');
+    const signInButton = screen.getByRole('button', { name: 'Create an account' });
+
+    userEvent.type(usernameInput, username);
+    userEvent.type(passwordInput, password);
+
+    userEvent.click(signInButton);
+
+    await waitFor(() => {
+        expect(screen.getByText('App')).toBeInTheDocument();
+    });
+});
+
+test('displays error message on unsuccessful sign in', async () => {
+
+});
