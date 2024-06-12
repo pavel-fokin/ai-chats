c6140cd65febeedf5f4e59bb7859280723b332a8
Pavel Fokin <fokinpv@gmail.com>
2024-06-12 17:23:20 +0200

    [fe] Use simple textarea with autorisize logic

--- apps/aibots-fe/src/components/TextArea.tsx
+++ apps/aibots-fe/src/components/TextArea.tsx
@@ -1,5 +1,3 @@
-import TextareaAutosize from 'react-textarea-autosize';
-
 import styles from './TextArea.module.css';
 
 type TextAreaProps = {
@@ -8,15 +6,30 @@ type TextAreaProps = {
   onChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
 };
 
-const TextArea = (props: TextAreaProps) => {
+import React, { useState } from 'react';
+
+export const TextArea: React.FC<TextAreaProps> = (props) => {
+  const [rows, setRows] = useState(1);
+  // const [text, setText] = useState('');
+
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    // const { value } = event.target;
+    // setText(value);
+
+    // Count the number of line breaks to adjust the rows
+    const lineBreaks = (props.value.match(/\n/g) || []).length + 1;
+    setRows(lineBreaks + 1);
+
+    props.onChange(event);
+  };
+
   return (
-    <TextareaAutosize
+    <textarea
+      rows={rows}
       value={props.value}
-      onChange={props.onChange}
+      onChange={handleChange}
       className={styles.TextArea}
       placeholder={props.placeholder}
     />
   );
 };
-
-export { TextArea };
